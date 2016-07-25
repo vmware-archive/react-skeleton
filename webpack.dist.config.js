157385bcf4611072c28537ea45b61965c321809a
Robert Gravina <robert.gravina@gmail.com>
2016-07-25 16:00:51 +0900

    Add production setting for webpack.dist.config
    React.js is one library which do things differently when NODE_ENV is 'production'.

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -1,7 +1,13 @@
 var webpack = require('webpack');
 var config = require('./webpack.config');
 
+
 config.plugins = config.plugins.concat([
+  new webpack.DefinePlugin({
+    'process.env':{
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
   new webpack.optimize.UglifyJsPlugin()
 ]);
 
